eda7c9cf9299fad873dff8ecff335e38a15f01f8
tank <povilas.daniusis@gmail.com>
2021-07-18 15:52:43 +0300

    Seismometer reading works.

--- firmware/v1/v1.ino
+++ firmware/v1/v1.ino
@@ -16,31 +16,13 @@ void setup() {
     Serial.println("ADS115 connected");
   }
   adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change parameter to change range
-  Serial.println("ADS1115 Example Sketch - Single Shot Mode");
-  Serial.println("Channel / Voltage [V]: ");
-  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   float voltage = 0.0;
-
   Serial.print("0: ");
-  voltage = readChannel(ADS1115_COMP_0_GND);
-  Serial.print(voltage);
-
-  Serial.print(",   1: ");
-  voltage = readChannel(ADS1115_COMP_1_GND);
-  Serial.print(voltage);
-  
-  Serial.print(",   2: ");
-  voltage = readChannel(ADS1115_COMP_2_GND);
-  Serial.print(voltage);
-
-  Serial.print(",   3: ");
-  voltage = readChannel(ADS1115_COMP_3_GND);
+  voltage = readChannel(ADS1115_COMP_0_1);
   Serial.println(voltage);
-
   //delay(10);
 }
 
@@ -49,6 +31,6 @@ float readChannel(ADS1115_MUX channel) {
   adc.setCompareChannels(channel);
   adc.startSingleMeasurement();
   while(adc.isBusy()){}
-  voltage = adc.getResult_V(); // alternative: getResult_mV for Millivolt
+  voltage = adc.getResult_mV(); // alternative: getResult_mV for Millivolt
   return voltage;
 }
