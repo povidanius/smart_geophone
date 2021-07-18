30357c0d6e00050132387c21143444cb961263e9
tank <povilas.daniusis@gmail.com>
2021-07-18 17:18:15 +0300

    meastured SM-24 sampling frequency to ~260Hz.

--- firmware/v1/v1.ino
+++ firmware/v1/v1.ino
@@ -4,6 +4,13 @@
 
 ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 
+/*
+ *  Geophone docs: http://cdn.sparkfun.com/datasheets/Sensors/Accelerometers/SM-24%20Brochure.pdf
+ * 
+ */
+
+unsigned long int frame_counter;
+
 void setup() {
   
   Wire.begin();
@@ -15,15 +22,28 @@ void setup() {
   {
     Serial.println("ADS115 connected");
   }
-  adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change parameter to change range
+  
+  adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change parameter to change range 
+  //adc.setVoltageRange_mV(ADS1115_RANGE_4096); //comment line/change parameter to change range
+  //adc.setVoltageRange_mV(ADS1115_RANGE_1024);
+  //adc.setVoltageRange_mV(ADS1115_RANGE_0512);
+  frame_counter = 0;
 }
 
 void loop() {
   float voltage = 0.0;
+  
   Serial.print("0: ");
   voltage = readChannel(ADS1115_COMP_0_1);
   Serial.println(voltage);
-  //delay(10);
+  //delay(10);  
+  
+  frame_counter++;
+  if (frame_counter % 1000 == 0)
+  {
+   // Serial.println((float) 1000* frame_counter/millis());
+  }  
+  
 }
 
 float readChannel(ADS1115_MUX channel) {
