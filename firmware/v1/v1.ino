2eac5edc7c91b47fa5ddf9bf9e1fe8bc983d79e4
tank <povilas.daniusis@gmail.com>
2021-07-20 23:18:20 +0300

    preparing to save data to files.

--- firmware/v1/v1.ino
+++ firmware/v1/v1.ino
@@ -1,8 +1,18 @@
 #include <ADS1115_WE.h>
 #include<Wire.h>
+#include <ArduinoQueue.h> // https://github.com/EinarArnason/ArduinoQueue
+
+
 #define I2C_ADDRESS 0x48
+#define QUEUE_SIZE  256
+#define TRIGGER_FRAME_COUNTER_INIT 25
 
 ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
+ArduinoQueue<float> readingQueue(QUEUE_SIZE); // approx one second.
+float trigger_voltage_mV = 100; // will save data if exceeded
+int trigger_counter = TRIGGER_FRAME_COUNTER_INIT; //~0.1 sec.
+bool trigger_activated;
+
 
 /*
  *  Geophone docs: http://cdn.sparkfun.com/datasheets/Sensors/Accelerometers/SM-24%20Brochure.pdf
@@ -28,6 +38,7 @@ void setup() {
   //adc.setVoltageRange_mV(ADS1115_RANGE_1024);
   //adc.setVoltageRange_mV(ADS1115_RANGE_0512);
   frame_counter = 0;
+  trigger_activated = false;
 }
 
 void loop() {
@@ -35,6 +46,20 @@ void loop() {
   
   Serial.print("0: ");
   voltage = readChannel(ADS1115_COMP_0_1);
+  readingQueue.enqueue(voltage);
+  if (fabs(voltage) > trigger_voltage_mV)  
+    trigger_activated = true;
+
+  if (trigger_activated) {
+    trigger_counter--;
+    if (trigger_counter == 0 && readingQueue.itemCount() == QUEUE_SIZE)
+    {
+        trigger_counter = TRIGGER_FRAME_COUNTER_INIT; 
+        trigger_activated = false;
+        saveData(); 
+    }
+  }
+  
   Serial.println(voltage);
   //delay(10);  
   
@@ -53,4 +78,9 @@ float readChannel(ADS1115_MUX channel) {
   while(adc.isBusy()){}
   voltage = adc.getResult_mV(); // alternative: getResult_mV for Millivolt
   return voltage;
+}
+
+// save queue to SD card
+void saveData() {
+  
 }
