58031081427e3953bc644e164e3911ad82f7466c
tank <povilas.daniusis@gmail.com>
2021-07-24 16:09:03 +0300

    saving data to sd card (not tested)

--- firmware/v1/v1.ino
+++ firmware/v1/v1.ino
@@ -1,18 +1,18 @@
 #include <ADS1115_WE.h>
 #include<Wire.h>
 #include <ArduinoQueue.h> // https://github.com/EinarArnason/ArduinoQueue
+#include <SD.h>
 
 
 #define I2C_ADDRESS 0x48
-#define QUEUE_SIZE  256
+#define QUEUE_SIZE  64
 #define TRIGGER_FRAME_COUNTER_INIT 25
 
 ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 ArduinoQueue<float> readingQueue(QUEUE_SIZE); // approx one second.
-float trigger_voltage_mV = 100; // will save data if exceeded
+float trigger_voltage_mV = 30; // will save data if exceeded
 int trigger_counter = TRIGGER_FRAME_COUNTER_INIT; //~0.1 sec.
 bool trigger_activated;
-
 
 /*
  *  Geophone docs: http://cdn.sparkfun.com/datasheets/Sensors/Accelerometers/SM-24%20Brochure.pdf
@@ -32,6 +32,14 @@ void setup() {
   {
     Serial.println("ADS115 connected");
   }
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  
   
   adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change parameter to change range 
   //adc.setVoltageRange_mV(ADS1115_RANGE_4096); //comment line/change parameter to change range
@@ -44,23 +52,29 @@ void setup() {
 void loop() {
   float voltage = 0.0;
   
-  Serial.print("0: ");
+ // Serial.print("0: ");
   voltage = readChannel(ADS1115_COMP_0_1);
   readingQueue.enqueue(voltage);
-  if (fabs(voltage) > trigger_voltage_mV)  
+  if (fabs(voltage) > trigger_voltage_mV && trigger_activated == false)  
+  {
     trigger_activated = true;
+    //Serial.println("Trigger activated");
+  }  
 
   if (trigger_activated) {
-    trigger_counter--;
-    if (trigger_counter == 0 && readingQueue.itemCount() == QUEUE_SIZE)
+    if (trigger_counter > 0)
+     trigger_counter--;   
+     
+    if (trigger_counter == 0 && readingQueue.isFull())
     {
         trigger_counter = TRIGGER_FRAME_COUNTER_INIT; 
         trigger_activated = false;
         saveData(); 
     }
   }
+  //Serial.println(readingQueue.itemCount());
   
-  Serial.println(voltage);
+  //Serial.println(voltage);
   //delay(10);  
   
   frame_counter++;
@@ -82,5 +96,39 @@ float readChannel(ADS1115_MUX channel) {
 
 // save queue to SD card
 void saveData() {
+  char file_name[16];
+  int idx = getAvailableFileNameIdx();
+
+  sprintf(file_name, "log_%d.txt", idx);  
+  Serial.println("Saving data to");
+  Serial.println(file_name);
   
+  File logFile = SD.open(file_name, FILE_WRITE);
+  while (!readingQueue.isEmpty())
+  {
+    float x = readingQueue.dequeue();   
+    logFile.println(x);
+  }
+
+  logFile.close();  
+  
+}
+
+int getAvailableFileNameIdx()
+{ 
+ char file_name[16];
+ int idx = 0;
+
+ while (1)
+ {
+  sprintf(file_name, "log_%d.txt", idx);
+  File myFile = SD.open(file_name);
+  if (!myFile) {
+    myFile.close();
+    return idx;
+  }
+  myFile.close(); 
+  idx++;
+ } 
+ 
 }
